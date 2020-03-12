edf87bb73b5fb69d2b721193603d3bfae74ae91e
Dean Ellis <dellis1972@googlemail.com>
2020-03-12 19:23:52 +0000

    ff

--- render.cmd
+++ render.cmd
@@ -1,13 +1,15 @@
 @echo off
-REM SET frames = %totalFrames% / %SYSTEM_TOTALJOBDINPHASE%
-REM SET offset = %SYSTEM_JOBPOSITIONINPHASE% - 1 * %frames%
-REM IF (
-REM     SET frameend = %offset% + %frames%
-REM ) ELSE (
-REM     SET frameend = %offset% + %frames% - 1
-REM )
 ECHO %BUILD_ARTIFACTSTAGINGDIRECTORY%
-ECHO %SYSTEM_TOTALJOBDINPHASE%
+ECHO %SYSTEM_TOTALJOBSINPHASE%
 ECHO %totalFrames%
 ECHO %SYSTEM_JOBPOSITIONINPHASE%
+SET frames = %totalFrames% / %SYSTEM_TOTALJOBDINPHASE%
+SET offset = %SYSTEM_JOBPOSITIONINPHASE% - 1 * %frames%
+IF (%SYSTEM_JOBPOSITIONINPHASE% == %SYSTEM_TOTALJOBSINPHASE%)
+    SET frameend = %offset% + %frames%
+) ELSE (
+    SET frameend = %offset% + %frames% - 1
+)
+ECHO %offset%
+ECHO %frameend%
 REM @blender\blender-2.82-windows64\blender.exe -b -d test.blend -noaudio -E CYCLES -x 1 -F PNG -o %BUILD_ARTIFACTSTAGINGDIRECTORY%\frame_#### -s %offset% -e %frameend% -a 
