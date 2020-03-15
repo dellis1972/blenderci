7ae8ce4dfbda5407e3072e493ef61becf94c8d2e
Dean Ellis <dellis1972@googlemail.com>
2020-03-15 20:47:15 +0000

    try a bash script

---
+++ render.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+let "frames = $totalFrames / $TotalJobsInPhase"
+let "offset = ($JobPositionInPhase - 1) * ${frames}"
+if [ $JobPositionInPhase -eq $TotalJobsInPhase ]
+then
+    let "frameend = ${offset}+${frames}"
+else
+    let "frameend= (${offset}+${frames})-1"
+fi
+echo "rendering from ${offset} to ${frameend} total jobs $TotalJobsInPhase job id $JobPositionInPhase"
+echo "blender -b test.blend -noaudio -E CYCLES -x 1 -F PNG -o 'output/frame_####' -s ${offset} -e ${frameend} -a"
+blender -b test.blend -noaudio -S Scene -E CYCLES -x 1 -F PNG -o "output/frame_####" -s ${offset} -e ${frameend} -a 
