4a4132b097f68225993e0cf438a94b3215e62029
Dean Ellis <dellis1972@googlemail.com>
2020-03-16 09:43:08 +0000

    pass blender as argument

--- render.sh
+++ render.sh
@@ -13,5 +13,6 @@ else
     let "frameend= (${offset}+${frames})-1"
 fi
 echo "rendering from ${offset} to ${frameend} total jobs $TotalJobsInPhase job id $JobPositionInPhase"
-echo "blender -b test.blend -noaudio -E CYCLES -x 1 -F PNG -o '${output}/frame_####' -s ${offset} -e ${frameend} -a"
-blender -b test.blend -noaudio -S Scene -E CYCLES -x 1 -F PNG -o "${output}/frame_####" -s ${offset} -e ${frameend} -a 
+echo "blender -b '$1' -noaudio -E CYCLES -x 1 -F PNG -o '${output}/frame_####' -s ${offset} -e ${frameend} -a"
+blender -b "$1" -noaudio -S Scene -E CYCLES -x 1 -F PNG -o "${output}/frame_####" -s ${offset} -e ${frameend} -a
+exit 0
